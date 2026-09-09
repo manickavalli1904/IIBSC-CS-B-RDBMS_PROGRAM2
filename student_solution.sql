2909370f3f628c44d3420138a61587e7c786ea9f
MANICKAVALLI S <manickavalliparvathy01@gmail.com>
2026-09-09 10:28:09 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,14 @@
-
+USE prgm1;
+CREATE TABLE Student
+(
+ StudentID INT(5)PRIMARY KEY,
+ StudentName VARCHAR(20) NOT NULL,
+ DOB DATE NOT NULL,
+ Gender VARCHAR(10) NOT NULL,
+ DepartmentID INT(5),
+ CONSTRAINT UQ_StudentName UNIQUE (StudentName),
+ CONSTRAINT FK_Department
+ FOREIGN KEY (DepartmentID)
+  REFERENCES Department(DepartmentID)
+);
+desc Student;
